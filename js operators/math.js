df85e60433c180e9e1e2cd0290cda9ed55fac6f4
soyammangla <soyammangla15@gmail.com>
2025-11-25 14:29:39 +0530

    Added js es features folder to js-es-features branch

--- js operators/math.js
+++
@@ -1,8 +0,0 @@
-let x = 16.75;
-let randomNum = Math.floor(Math.random() * 41) + 10; // 10-50
-
-console.log(`Number: ${x}`);
-console.log(`Rounded: ${Math.round(x)}`);
-console.log(`Square Root: ${Math.sqrt(x).toFixed(2)}`);
-console.log(`Cubed: ${Math.pow(x, 3).toFixed(2)}`);
-console.log(`Random Number (10-50): ${randomNum}`);
--- js operators/oddeven.js
+++
@@ -1,9 +0,0 @@
-let results = [];
-
-for (let i = 1; i <= 30; i++) {
-  if (i % 3 === 0 && i % 5 === 0) results.push("FizzBuzz");
-  else if (i % 2 === 0) results.push("Even");
-  else results.push("Odd");
-}
-
-console.log(results);
